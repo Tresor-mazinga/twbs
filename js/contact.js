6b32d221714909dc26ae5d52fc8e259dabfbe87e
Tresor Mazinga <mazingatresor3@gmail.com>
2020-07-24 17:40:51 +0200

    first commit

---
+++ js/contact.js
@@ -0,0 +1,38 @@
+function nameProperty()
+{
+    var nameField = document.getElementById("name");
+    nameField.style.border = "1px solid orange";
+}
+
+function nameDefault()
+{
+    var nameField = document.getElementById("name");
+    nameField.style.border = "none";
+    nameField.style.borderBottom = "1px solid grey";
+}
+
+function emailProperty()
+{
+    var emailField = document.getElementById("email");
+    emailField.style.border = "1px solid orange";
+}
+
+function emailDefault()
+{
+    var emailField = document.getElementById("email");
+    emailField.style.border = "none";
+    emailField.style.borderBottom = "1px solid grey";
+}
+
+function textAreaProperty()
+{
+    var textAreaField = document.getElementById("text_area");
+    textAreaField.style.border = "1px solid orange";
+}
+
+function textAreaDefault()
+{
+    var textAreaField = document.getElementById("text_area");
+    textAreaField.style.border = "none";
+    textAreaField.style.borderBottom = "1px solid grey";
+}
